71a527501181fa107b192442dd6be276a796912a
Riyanda Azis Febrian <riyandaazis00@gmail.com>
2023-11-21 23:56:36 +0700

    Rebuilt App
    Rebuilt because of failed to implement Recyclerview with Navigation

--- app/src/main/java/com/usu/kelasusumobile/MainActivity.kt
+++ app/src/main/java/com/usu/kelasusumobile/MainActivity.kt
@@ -2,13 +2,19 @@ package com.usu.kelasusumobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.usu.kelasusumobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
